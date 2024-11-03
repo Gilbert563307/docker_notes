582c4604f38c663a5bc6dad22d5523dfc3b5c98a
Gilbert563307 <ssempijja759@gmail.com>
2024-11-04 00:29:46 +0100

    theme button changed

--- src/view/settings/components/ThemeModeComponent.jsx
+++ src/view/settings/components/ThemeModeComponent.jsx
@@ -9,7 +9,7 @@ export default function ThemeModeComponent() {
         <div className='setting-option'>
 
             <div className="dropdown">
-                <button className="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <button className="btn btn-dark btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Theme
                 </button>
                 <ul className="dropdown-menu">
