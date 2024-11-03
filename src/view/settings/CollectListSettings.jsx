cb9c52731ba8ca16f1c2e1aef06cacf40b817043
Gilbert563307 <ssempijja759@gmail.com>
2024-11-04 00:48:29 +0100

    added logout btn

--- src/view/settings/CollectListSettings.jsx
+++ src/view/settings/CollectListSettings.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import useSetPageTitleHook from '../../hooks/useSetPageTitleHook';
 import ThemeModeComponent from './components/ThemeModeComponent';
 import "../../assets/css/views/CollectListSettings.css";
+import { useAuthProvider } from '../../context/AuthProvider';
 
 export default function CollectListSettings() {
   useSetPageTitleHook({ title: "Settings " });
-
+  const { logout } = useAuthProvider();
   return (
     <article className='settings-options'>
       <ThemeModeComponent />
+      <button type="button" className="btn btn-link" onClick={logout}>Logout</button>
+
     </article>
   )
 }
