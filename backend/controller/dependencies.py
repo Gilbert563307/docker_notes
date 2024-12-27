29497ec40460bc770be2ddfc92ac904d44491e16
Gilbert563307 <94962686+Gilbert563307@users.noreply.github.com>
2024-12-27 04:09:46 +0100

    minor fix

--- backend/controller/dependencies.py
+++ backend/controller/dependencies.py
@@ -8,7 +8,7 @@ load_dotenv()
 async def get_token_header(x_token: Annotated[str, Header()] = ""):
     return SessionLogic.is_token_valid(x_token)
 
-
+#TODO do not use
 async def get_query_token(token: str):
     if token != "SECRET_TOKEN" or token == "":
         raise HTTPException(status_code=400, detail="No token | wrong token provided")
