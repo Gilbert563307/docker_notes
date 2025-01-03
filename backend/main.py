d62f5242ea1c20f2417842fbdc54fc12291d3544
Gilbert563307 <94962686+Gilbert563307@users.noreply.github.com>
2025-01-03 03:25:08 +0100

    minor fix

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,11 @@ app = FastAPI()
 app.include_router(files_router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://tasks-tracker.online"],  # Replace "*" with your React app's URL in production
+    allow_origins=[
+        "http://localhost:5173",
+        "https://tasks-tracker.online",
+        "https://tasks-tracker.online/api/",
+    ],  # Replace "*" with your React app's URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
