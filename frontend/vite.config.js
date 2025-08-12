fb7eae5123fbe79bdd36474e0599e0c89b595e57
Gilbert563307 <94962686+Gilbert563307@users.noreply.github.com>
2025-08-12 14:01:13 +0200

    updated vite vonfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //Production notes
+  //When reverse proxying under /app, you must set Vite’s base option so it knows to prepend /app/ to all asset paths.
+  base: "/app/",
   plugins: [react()],
   server: {
     host: true,
@@ -10,7 +13,7 @@ export default defineConfig({
     // Thanks @sergiomoura for the window fix
     // add the next lines if you're using windows and hot reload doesn't work
     watch: {
-      usePolling: true
-    }
-  }
-})
+      usePolling: true,
+    },
+  },
+});
