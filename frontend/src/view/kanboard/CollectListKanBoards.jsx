41c2a42c3fceae5657230dd02403f561a79f834c
Gilbert563307 <dragonboy4445@gmail.com>
2025-09-04 14:36:36 +0200

    minor issues

--- frontend/src/view/kanboard/CollectListKanBoards.jsx
+++ frontend/src/view/kanboard/CollectListKanBoards.jsx
@@ -10,21 +10,32 @@ export default function CollectListKanBoards() {
   useSetPageTitleHook({ title: "Kanban" });
 
   return (
-    <div className="cards">
-      {boards.map((item) => {
-        const boardsUrl = `/board/${item.id}`;
-        return (
-          <Link to={boardsUrl}>
-            <div key={item.id} className="card">
-              <div
-                className="card-color"
-                style={{ backgroundColor: item.color }}
-              />
-              <div className="card-label">{item.name}</div>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
+    <article>
+      <div>
+        <Link
+          aria-describedby="create task button"
+          className="add-task-button task-btn-plain "
+          to="/kanboards/create"
+        >
+          create
+        </Link>
+      </div>
+      <div className="cards">
+        {boards.map((item) => {
+          const boardsUrl = `/board/${item.id}`;
+          return (
+            <Link to={boardsUrl}>
+              <div key={item.id} className="card">
+                <div
+                  className="card-color"
+                  style={{ backgroundColor: item.color }}
+                />
+                <div className="card-label">{item.name}</div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </article>
   );
 }
